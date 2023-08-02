8f63593f32975d936937e17ceac645872357fe0e
gitk01n <83260491+gitk01n@users.noreply.github.com>
2023-08-02 20:24:54 +0800

    momo.js

---
+++ momo.js
@@ -0,0 +1,11 @@
+[rewrite_local]
+^https:\/\/biz-vip\.immomo\.com\/vCenter\/home\/index\?mkVersion=0 script-response-body https://raw.githubusercontent.com/gitk01n/nothing/main/momo.js
+
+[mitm]
+hostname = *.immomo.com
+
+let obj=JSON.parse($response.body)
+obj.result["svipExpireTime"]=4096483190
+obj.result["buySvip"]=true
+obj.result["svipYear"]=1
+$done({body:JSON.stringify(obj)})
